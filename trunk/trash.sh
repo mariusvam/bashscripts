052b12f279e94bd300dd36bec59d8ab228ab8d27
trile7@gmail.com <trile7@gmail.com@a4d702ef-8957-79ee-d4a0-1d4fe1a1462e>
2012-02-26 14:23:38 +0000

    change naming convention

--- trunk/trash.sh
+++ trunk/trash.sh
@@ -1,62 +1,73 @@
 #!/bin/bash
+# author: trile7 at gmail dot com
 
-icon="/home/tri/icons/trash.png"
-trashcan="$HOME/.local/trash"
-max_size=1000000000
-d="%fs%"
+trashcan="$HOME/.local/share/Trash/files"
+maxsize=1000000000 #in byte.  Leave blank to ignore file size
 
-function usage() {
-cat << EOF
-Dependencies: yad
-
-Usage:
-  -h|--help|--usage|--info     print this message
-  --empty                      empty everything in $trashcan
-  --restore filepaths          restore selected files to it original location
-  filepaths                    move files to $trashcan
-
-When a file is move to the trashcan by this script, the directory indicator forward slash is converted to "$d".  This is necessary if one want to restore the file to its original location.
-
-Author: trile7 at gmail dot com
-EOF
-exit
-}
-
-[ $# -eq 0 ] && usage
-mkdir -p $trashcan
+if ! mkdir -p "$trashcan"; then
+  echo "cannot create trashcan"
+  exit 1
+fi
 case $1 in
-  -h|--help|--usage|--info) usage ;;
-  --empty) rm -rf $trashcan/* ;;
+  -h|--help|--usage|--info)
+    echo "$0 filepath1 filepath2 ...            #move file to $trashcan"
+    echo "$0 --restore filepath1 filepath2 ...  #restore item to its orginal location"
+    echo "$0 --empty                            #remove all items in $trashcan"
+    echo
+    echo "trashcan location $trashcan"
+    echo
+    echo "When a file is move to trashcan using this script, a .filename.info containing the filepath and deletion date is created in the trashcan.  The script uses this info file with the --restore option."
+    echo
+    echo "When trash or restore a file, if the destination already have the same filename, _1 is added to the new filename." ;;
+  --empty)
+    cd "$trashcan"
+    rm -rf * .??* ;;
   --restore)
     shift
     for i; do
-      f=${i##*/}
-      f=${f//$d/\/}
-      if [ -e "$f" ]; then
-        yad --center --on-top --window-icon $icon gtk-delete --button gtk-no:1 --button gtk-yes:0 --title "Confirm Overwrite" --text "$f existed.  Do you want to overwrite?"
-        [ $? -eq 0 ] && mv -f "$i" "$f"
+      infofile=`dirname "$1"`/.`basename "$i"`.info
+      if [[ -f "$infofile" ]]; then
+        filepath=`head -n1 $infofile`
       else
-        mv "$i" "$f"
+        echo "cannot restore $i due to $infofile missing"
+        continue
       fi
+      targetfolder=`dirname "$filepath"`
+      while [[ -e "$filepath" ]]; do
+        (( j++ ))
+        filepath="$filepath"_1
+        if [[ $j -gt 100 ]]; then
+          echo "cannot restore $i due to too many conflicts in $targetfolder"
+          break
+        fi
+      done
+      [[ -e "$filepath" ]] && continue
+      mkdir -p "$targetfolder" && mv -n "$i" "$filepath" && rm "$infofile"
     done ;;
   *)
     for i; do
-      [[ $trashcan = ${i%/*} ]] && continue
-      s=(`du -bs "$i"`)
-      if [ $s -gt $max_size ]; then
-        yad --center --on-top --window-icon $icon gtk-delete --button gtk-no:1 --button gtk-yes:0 --title "Confirm Delete" --text "$i size is greater than $max_size.  Delete permanently? \n<b>No</b> will move it to trash"
-        ret=$?
-      else
-        ret=1
+      if [[ `dirname "$i"` = "$trashcan" ]]; then
+        echo "cannot trash $i because it's already in the trashcan"
+        continue
       fi
-      if [[ $ret -eq 0 ]]; then
-        rm -rf "$i"
-      elif [ $ret -eq 1 ]; then
-        cd `dirname "$i"`
-        f=${PWD//\//$d}$d${i##*/}
-        mv -f "$i" $trashcan/"$f"
-      else
-        exit $ret
+      if [[ $i != /* ]]; then
+        echo "full filepath is needed for $i"
+        continue
       fi
+      if [[ $maxsize ]] && [[ `du -bs "$i" | awk '{print $1}'` -gt $maxsize ]]; then
+        echo "skip $i because it's larger than $maxsize"
+        continue
+      fi
+      filename=`basename "$i"`
+      while [[ -e "$trashcan/$filename" ]]; do
+        (( j++ ))
+        filename="$filename"_1
+        if [[ $j -gt 100 ]]; then
+          echo "cannot trash $i due to conflict in $trashcan"
+          break
+        fi
+      done
+      [[ -e "$trashcan/$filename" ]] && continue
+      mv -n "$i" "$trashcan/$filename" && echo -e "$i\ndeleted on `date`" > "$trashcan/.$filename.info"
     done ;;
 esac
